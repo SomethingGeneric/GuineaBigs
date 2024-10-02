8ec3d672e039ea1fd239e92b5f94a9f17480cd5c
matt c <matt@xhec.dev>
2024-10-02 22:54:59 +0100

    naturally occuring friends

--- src/main/java/cloud/goober/guineabigs/GuineaBigs.java
+++ src/main/java/cloud/goober/guineabigs/GuineaBigs.java
@@ -1,6 +1,8 @@
 package cloud.goober.guineabigs;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
@@ -10,6 +12,8 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
 import net.minecraft.util.Identifier;
+import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.biome.SpawnSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,6 +30,18 @@ public class GuineaBigs implements ModInitializer {
 				.build()
 	);
 
+	private void addEntityToBiomes() {
+		// Add spawn settings for Grassland and Flower Fields biomes
+		BiomeModifications.create(Identifier.of(MOD_ID, "entity_spawns"))
+				.add(ModificationPhase.ADDITIONS, context -> context.getBiomeKey() == BiomeKeys.PLAINS || context.getBiomeKey() == BiomeKeys.FLOWER_FOREST,
+						context -> {
+							context.getSpawnSettings().addSpawn(
+									SpawnGroup.CREATURE, // Choose the spawn group (CREATURE, MONSTER, etc.)
+									new SpawnSettings.SpawnEntry(GuineaBigs.GUINEA_PIG, 14, 5, 15) // Entity, weight, min group size, max group size
+							);
+						});
+	}
+
 	// Start methods
 	@Override
 	public void onInitialize() {
@@ -36,5 +52,7 @@ public class GuineaBigs implements ModInitializer {
 		LOGGER.info("Blocks initialized");
 		FabricDefaultAttributeRegistry.register(GUINEA_PIG, GuineaPigEntity.createGuineaPigAttributes());
 		LOGGER.info("The boys have been added");
+		addEntityToBiomes();
+		LOGGER.info("Set up entity spawn rules");
 	}
 }
