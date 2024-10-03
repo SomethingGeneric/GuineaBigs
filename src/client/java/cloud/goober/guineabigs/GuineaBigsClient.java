e6900af56f9a6177a963709d50aeb4b8d12d97db
matt c <matt@xhec.dev>
2024-10-03 16:19:29 +0100

    custom model for the friends!

--- src/client/java/cloud/goober/guineabigs/GuineaBigsClient.java
+++ src/client/java/cloud/goober/guineabigs/GuineaBigsClient.java
@@ -2,41 +2,43 @@ package cloud.goober.guineabigs;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
-import net.minecraft.client.render.entity.model.EntityModelLayers;
-import net.minecraft.client.render.entity.model.PigEntityModel;
+import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 
 public class GuineaBigsClient implements ClientModInitializer {
-	@Override
-    public void onInitializeClient() {
+    public static final EntityModelLayer GUINEA_MODEL_LAYER = new EntityModelLayer(Identifier.of(GuineaBigs.MOD_ID, "guinea_pig"), "main");
 
+    @Override
+    public void onInitializeClient() {
         // Transparent poops
         BlockRenderLayerMap.INSTANCE.putBlock(GuineaBlocks.GUINEA_PELLET, RenderLayer.getCutout());
 
-        // Creature
-        // TODO: He shouldn't be a reskinned pig model lol
+        // Register entity renderer with custom model
         EntityRendererRegistry.register(GuineaBigs.GUINEA_PIG, (EntityRendererFactory.Context context) ->
-                new MobEntityRenderer<GuineaPigEntity, PigEntityModel<GuineaPigEntity>>(
+                new MobEntityRenderer<GuineaPigEntity, GuineaModel>(
                         context,
-                        new PigEntityModel<>(context.getPart(EntityModelLayers.PIG)),
+                        new GuineaModel(context.getPart(GUINEA_MODEL_LAYER)),  // Use your custom model
                         0.25f // Shadow radius (halved from 0.5f)
                 ) {
                     @Override
                     public Identifier getTexture(GuineaPigEntity entity) {
-                        return Identifier.of(GuineaBigs.MOD_ID, "textures/entity/pig.png");
+                        return Identifier.of(GuineaBigs.MOD_ID, "textures/entity/guinea_pig.png"); // Update texture
                     }
 
                     @Override
                     protected void scale(GuineaPigEntity entity, MatrixStack matrixStack, float f) {
-                        // Scale the entity to half its size
-                        matrixStack.scale(0.25f, 0.5f, 0.25f);
+                        matrixStack.scale(0.5f, 0.5f, 0.5f); // Scale to fit your custom model
                     }
                 }
         );
+
+        EntityModelLayerRegistry.registerModelLayer(GuineaBigsClient.GUINEA_MODEL_LAYER, GuineaModel::getTexturedModelData);
+
     }
 }
