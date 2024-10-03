f4d6a1f4265bbdcda87a2da739ccce2755d2079e
matt c <matt@xhec.dev>
2024-10-03 12:15:17 +0100

    guinea pellets actually look like poops now

--- src/client/java/cloud/goober/guineabigs/GuineaBigsClient.java
+++ src/client/java/cloud/goober/guineabigs/GuineaBigsClient.java
@@ -1,7 +1,9 @@
 package cloud.goober.guineabigs;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.render.entity.model.EntityModelLayers;
@@ -12,6 +14,11 @@ import net.minecraft.util.Identifier;
 public class GuineaBigsClient implements ClientModInitializer {
 	@Override
     public void onInitializeClient() {
+
+        // Transparent poops
+        BlockRenderLayerMap.INSTANCE.putBlock(GuineaBlocks.GUINEA_PELLET, RenderLayer.getCutout());
+
+        // Creature
         // TODO: He shouldn't be a reskinned pig model lol
         EntityRendererRegistry.register(GuineaBigs.GUINEA_PIG, (EntityRendererFactory.Context context) ->
                 new MobEntityRenderer<GuineaPigEntity, PigEntityModel<GuineaPigEntity>>(
@@ -21,7 +28,7 @@ public class GuineaBigsClient implements ClientModInitializer {
                 ) {
                     @Override
                     public Identifier getTexture(GuineaPigEntity entity) {
-                        return Identifier.of(GuineaBigs.MOD_ID, "textures/entity/guinea_pig.png");
+                        return Identifier.of(GuineaBigs.MOD_ID, "textures/entity/pig.png");
                     }
 
                     @Override
